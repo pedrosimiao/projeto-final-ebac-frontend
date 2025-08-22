106512a7b8ca7952abf9c3f39a9c1d5f3509f1e1
pedrosimiao <pedrolumiere@gmail.com>
2025-08-21 21:27:02 -0300

    fix: FeedContainer

--- src/components/FeedContent/styles.ts
+++ src/components/FeedContent/styles.ts
@@ -3,9 +3,11 @@
 import styled from "styled-components";
 
 export const FeedContainer = styled.main`
+  /* width: 600px; */
   flex: 0 1 600px;
+  max-width: 100%;
   min-height: 100vh;
-  flex-grow: 0;
+  /* flex-grow: 0; */
   position: relative;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
