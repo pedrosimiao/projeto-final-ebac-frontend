8cd11266621f377a53611e1caa8f2abad67f9cec
pedrosimiao <pedrolumiere@gmail.com>
2025-08-21 21:24:38 -0300

    fix: FeedContainer

--- src/components/FeedContent/styles.ts
+++ src/components/FeedContent/styles.ts
@@ -3,12 +3,12 @@
 import styled from "styled-components";
 
 export const FeedContainer = styled.main`
-  width: 600px;
+  flex: 0 1 600px;
   min-height: 100vh;
   flex-grow: 0;
   position: relative;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    width: 100%;
+    flex-basis: 100%;
   }
 `;
